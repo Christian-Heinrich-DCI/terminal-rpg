e0c162cdb6c9ea84d1b4be8307b4d571b63e7026
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2023-02-22 16:15:22 +0100

    end of day, only up-attacks work

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Terminal RPG
+
+## Steuerung (WASD)
+
+-  w - hoch
+-  a - links
+-  s - runter
+-  d - rechts
+-  q - quit
+
+## Benutze Pakete
+
+-  readline-sync (https://www.npmjs.com/package/readline-sync)
+-  chalk (https://www.npmjs.com/package/chalk)
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+import readlineSync from "readline-sync";
+import chalk from "chalk";
+
 // ---------- CONFIG ----------
 
+const title = "DUNGEON OF CODE";
 const cols = 80;
 const rows = 24;
 
@@ -10,6 +14,11 @@ class Creature {
       this.symbol = symbol;
       this.x = x;
       this.y = y;
+      this.hp = 3;
+   }
+   hit(target) {
+      if (target.hp > 0) target.hp--;
+      if (target.hp === 0) player.xp++;
    }
 }
 
@@ -17,14 +26,25 @@ class Player extends Creature {
    constructor(symbol, x, y, playerClass) {
       super(symbol, x, y);
       this.playerClass = playerClass;
-      this.hp = playerClass === "Fighter" ? 10 : 6;
+      this.maxHp = playerClass === "Fighter" ? 10 : 6;
+      this.hp = this.maxHp;
+      this.maxMp = playerClass === "Fighter" ? 0 : 4;
+      this.mp = this.maxMp;
       this.xp = 0;
+      this.level = 1;
+   }
+}
+
+class Attack {
+   constructor(name, type, damage, cost) {
+      this.name = name;
+      this.type = type;
+      this.damage = damage;
+      this.cost = cost;
    }
 }
 
 // ---------- HELPER FUNCTIONS ----------
-
-const title = " DUNGEON OF CODE ";
 
 // centers string, based on width of terminal (cols)
 // fills rest with
@@ -52,30 +72,107 @@ function render() {
       let rowOutput = "";
       for (let x = 0; x < cols; x++) {
          if (x === player.x && y === player.y) {
-            rowOutput += player.symbol;
+            rowOutput += chalk.bold.white(player.symbol);
          } else if (x === monster.x && y === monster.y) {
-            rowOutput += monster.symbol;
+            if (monster.hp > 0) rowOutput += chalk.bold.red(monster.symbol);
+            else rowOutput += chalk.bold.dim(monster.symbol);
          } else {
-            rowOutput += ".";
+            rowOutput += chalk.gray(".");
          }
       }
       console.log(rowOutput);
    }
 
    // RENDER: Status Bar
+   // * Class
+   const renderClass = `Class: ${player.playerClass}`;
+   // * Level
+   const renderLevel = `Lvl: ${player.level}`;
+   // * Armor
+   const renderArmor = `🛡️  Chain Mail`;
+   // * Weapon
+   const renderWeapon = `🗡️  Longsword`;
+   // * HitPoints
+   let renderHP = "HP: ";
+   if (player.hp === player.maxHp) renderHP += player.hp;
+   else if (player.hp > player.maxHp * 0.67) renderHP += chalk.green(player.hp);
+   else if (player.hp > player.maxHp * 0.33)
+      renderHP += chalk.yellow(player.hp);
+   else if (player.hp > 0) renderHP += chalk.red(player.hp);
+   else renderHP += "💀";
+   // * Experience
+   const renderXP = `XP: ${player.xp}`;
+   // * Menü
+   const renderQuit = "[q]uit";
 
-   console.log(
-      ` Class: ${player.playerClass} | Lvl: 1 | 🛡️  Chain Mail | 🗡️  Longsword | HP: ${player.hp} | XP: 0`
-   );
+   const output = [
+      renderClass,
+      renderLevel,
+      renderArmor,
+      renderWeapon,
+      renderHP,
+      renderXP,
+      renderQuit,
+   ].join(chalk.dim(" | "));
+
+   console.log(output);
 }
 
 // ---------- INITIALIZATION ----------
 
-const monster = new Creature("M", 20, 6);
+// Creatures
+const monster = new Creature("O", 38, 10);
+// const goblin = new Creature("G", 32, 14);
+// const monster = [ork, goblin];
 const player = new Player("@", 40, 12, "Fighter");
+
+// Attacks
+const spark = new Attack("Spark", "magical", "1D4", 1);
+const longsword = new Attack("Longsword", "physical", "1D8", 0);
 
 // ToDo: random position of monster(s)
 
 // ---------- START GAME ----------
 
-render();
+function checkColision(direction) {
+   switch (direction) {
+      case "left":
+         // something
+         break;
+      case "up":
+         if (player.x === monster.x && player.y - 1 === monster.y) {
+            player.hit(monster);
+            monster.hit(player);
+         } else player.y--;
+         break;
+      default:
+         // something
+         break;
+   }
+}
+
+while (true) {
+   render();
+   let key = readlineSync.keyIn("", {
+      hideEchoBack: true,
+      mask: "",
+      limit: "wasdtq",
+   });
+   if (key === "w") {
+      checkColision("up");
+   } else if (key === "a") {
+      player.x--;
+   } else if (key === "s") {
+      player.y++;
+   } else if (key === "d") {
+      player.x++;
+   } else if (key === "t") {
+      // Test
+      monster.hit(player);
+   } else if (key === "q") {
+      break;
+   }
+   //  else {
+   //    break;
+   // }
+}
